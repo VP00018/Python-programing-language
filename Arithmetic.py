1ffc1a4ffd0adb1fb0d1fb67e3ba08af591fb6d4
varadpandharkar <varadpandharkar89@gmail.com>
2026-03-11 23:19:44 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,34 @@
+"""
+Friends = 8
+
+#Friends += 1
+#Friends -= 1
+#Friends *= 3
+#Friends **= 2
+
+
+print(Friends)
+
+"""
+"""
+x = 3.14
+y = 5
+z = 3
+# result = round (x)
+# result = abs(y)
+# result = pow(4, 3)
+# result = max(x,y,z)
+# result = min(x,y,z)
+print(result)
+
+"""
+
+import math
+
+x = 9.1
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)
+result = math.floor(x)
+print(result)
