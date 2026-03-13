5716e2c36bec6d5af5d7d1fc8914dbdeafef437b
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-13 23:25:46 +0530

    Add files via upload

---
+++ Conditional_Exp.py
@@ -0,0 +1,17 @@
+# X if Condition else Y
+
+num = 6
+a = 6
+b = 7
+age = 8
+temp = 20
+
+# print("Positive" if num > 0 else "Negative")
+
+result = ("Even" if num % 2 == 0 else "Odd")
+max_num = a if a > b else b
+min_num = a if a < b else b
+status = ("Adult" if age >= 18 else "Child")
+Weather = ("Hot" if temp > 20 else "Cold")
+
+print(Weather)
