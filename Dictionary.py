f4e41d50f2f7e99bc975a23303215b2e1ea0163c
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-18 21:34:22 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,27 @@
+
+Capitals = {"USA": "Washington D.C",
+            "INDIA": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+#print(Capitals.get("Japan"))
+
+# if Capitals.get("Russia"):
+#       print("That Capital Exists")
+# else:
+#       print("That capital doesn't exist")
+
+# Capitals.update({"Germany": "Berlin"})
+# Capitals.update({"USA": "Detroit"})
+# Capitals.pop("China")
+# Capitals.clear()
+# Capitals.popitem()
+
+keys = Capitals.keys()
+
+for key in Capitals.keys():
+    print(key)
+
+
+
+
