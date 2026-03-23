935c75b812011702459449be6a9eeb3c91188fac
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-23 21:17:12 +0530

    Add files via upload

---
+++ Membership_operator.py
@@ -0,0 +1,19 @@
+"""
+word = "APPLE"
+letter = input("Guess a letter in the secret word: ")
+
+if letter in word:
+    print(f"There is a {letter}")
+else:
+    print(f"{letter} was not found")
+
+    """
+
+students = {"Varad", "Yugank", "Bhavesh"}
+
+student = input("Enter the name of student: ")
+
+if student in students:
+    print(f"{student} is a student")
+else:
+    print(f"{student} was not found")
